c75171de7bb4e4c55874d96c4fd807b751a5742c
bendtherules <abhasbhattacharya2@gmail.com>
2019-12-15 16:41:56 +0530

    Clean - Remove useless scaffolded files + Fix index export

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -1,5 +1,5 @@
 import * as index from '../src/index';
 
-test('Should have Greeter available', () => {
-  expect(index.Greeter).toBeTruthy();
+test('Should have render available', () => {
+  expect(index.render).toBeTruthy();
 });
